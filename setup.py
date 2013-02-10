7c6c7d872433d6eddeea6fa51d8b782c5d2cdc18
Matthieu Monsch <ninecoast@gmail.com>
2013-02-10 13:38:13 -0500

    Doc.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='flasker',
-    version='0.1.25',
+    version='0.1.26',
     description='Flask, SQLAlchemy, and Celery integration',
     long_description=open('README.rst').read(),
     author='Matthieu Monsch',
