f71db22b334dd2a86617b85cfb0132c40b2ff77e
Matthieu Monsch <ninecoast@gmail.com>
2013-02-26 16:16:00 -0500

    Skl storage.

--- flasker/ext/skl.py
+++ flasker/ext/skl.py
@@ -4,8 +4,11 @@ from datetime import datetime
 from time import time
 from flasker.ext.orm import Model
 from flasker.util import JSONEncodedDict
+from os.path import join
+from pandas import DataFrame, Series
+from sklearn.externals import joblib
 from sqlalchemy import (Boolean, Column, Float, ForeignKey, Integer, String,
-  DateTime, Numeric)
+  DateTime, Numeric, Text)
 from sqlalchemy.orm import backref, relationship
 from sys import modules
 
@@ -16,15 +19,20 @@ class SKLErr(Exception):
 
 class SKL(object):
 
+  __state = {}
+  __registered = False
+
   config = {
     'FOLDER': 'skl'
   }
 
   def __init__(self):
-    pass
+    if not self.__registered:
+      self.__dict__ = self.__state
+      self.__registered = True
 
   def on_register(self, project):
-    pass
+    self.folder_path = join(project.root_dir, self.config['FOLDER'])
 
 
 # Models
@@ -34,6 +42,7 @@ class ClassifierParam(Model):
   _cache = None
   id = Column(Integer, primary_key=True)
   type = Column(String(64))
+  description = Column(Text)
   added = Column(DateTime, default=datetime.now)
   penalty = Column(String(32))
   dual = Column(Boolean)
@@ -76,7 +85,9 @@ class LogisticRegressionParam(ClassifierParam):
 
 class ClassifierFit(Model):
 
+  _cache = None
   id = Column(Integer, primary_key=True)
+  description = Column(Text)
   added = Column(DateTime, default=datetime.now)
   duration = Column(Float)
   param_id = Column(ForeignKey('classifier_params.id'))
@@ -86,21 +97,41 @@ class ClassifierFit(Model):
     backref=backref('fits')
   )
 
+  @property
+  def filepath(self):
+    return join(SKL().folder_path, 'fit', '%s.joblib.pkl' % self.id)
+
   def get_fitted_classifier(self):
-    pass
+    return Classifier(joblib.load(self.filepath))
 
 
-class ClassifierPredict(Model):
+class ClassifierTest(Model):
 
+  _cache = None
   id = Column(Integer, primary_key=True)
+  fit_id = Column(ForeignKey('classifier_fits.id'))
+  description = Column(Text)
   added = Column(DateTime, default=datetime.now)
   duration = Column(Float)
-  fit_id = Column(ForeignKey('classifier_fits.id'))
+  _results = None
 
   fit = relationship(
     'ClassifierFit',
-    backref=backref('predicts')
+    backref=backref('tests')
   )
+
+  @property
+  def filepath(self):
+    return join(SKL().folder_path, 'test', '%s.pkl' % self.id)
+
+  @property
+  def results(self):
+    if self._results is None:
+      self._results = DataFrame.load(self.filepath)
+    return self._results
+
+  def save(self):
+    self._results.save(self.filepath)
 
 
 # Classifiers
@@ -109,33 +140,67 @@ class Classifier(object):
 
   """Adds tracking to classifier methods."""
 
-  def __init__(self, engine_factory, **kwargs):
-    self.engine = engine_factory(**kwargs)
+  def __init__(self, engine, **kwargs):
+    if callable(engine): # this is a factory
+      self.engine = engine(**kwargs)
+    else:
+      self.engine = engine
     self.param, self.flag = ClassifierParam.retrieve(
-      type='%s.%s' % (engine_factory.__module__, engine_factory.__name__),
+      type='%s.%s' % (
+        self.engine.__module__,
+        self.engine.__class__.__name__
+      ),
       **self.engine.get_params()
     )
     if self.flag:
       self.param.flush()
     self.current_fit = None
 
-  def fit(self, *args, **kwargs):
+  def is_fitted(self):
+    return self.current_fit is not None
+
+  def train(self, Xdf, ys, description=''):
+    """Fit then test in sample.
+
+    :param Xdf: features
+    :type Xdf: pandas.DataFrame
+    :param ys: labels
+    :type ys: pandas.Series
+
+    """
+    self._fit(Xdf.values, ys.values, description)
+    return self.test(Xdf, ys, 'in sample test')
+
+  def test(self, Xdf, ys, description=''):
+    """Test.
+
+    :param Xdf:
+    :type Xdf: pandas.DataFrame
+    :param ys: optional truth
+    :type ys: pandas.Series
+
+    """
+    if not self.is_fitted():
+      raise SKLError('Classifier must be trained before testing.')
     now = time()
-    fit = ClassifierFit(param=self.param)
-    self.engine.fit(*args, **kwargs)
+    test = ClassifierTest(fit=self.current_fit, description=description)
+    prediction = self.engine.predict(Xdf.values)
+    test.duration = time() - now
+    test._results = DataFrame({'truth': ys, 'prediction': prediction})
+    test.flush()
+    test.save()
+    return test
+
+  def set_description(self, description):
+    self.param.description = description
+
+  def _fit(self, X, y, description):
+    now = time()
+    fit = ClassifierFit(param=self.param, description=description)
+    self.engine.fit(X, y)
     fit.duration = time() - now
     fit.flush()
+    joblib.dump(self.engine, fit.filepath)
     self.current_fit = fit
-    # TODO save fitted classifier
-
-  def predict(self, *args, **kwargs):
-    if self.current_fit is None:
-      raise SKLError('Classifier must be fit before predicting.')
-    now = time()
-    predict = ClassifierPredict(fit=self.current_fit)
-    prediction = self.engine.predict(*args, **kwargs)
-    predict.duration = time() - now
-    predict.flush()
-    # TODO save prediction results
-    return prediction
+    
 
