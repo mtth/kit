14b36d5d4fb8fa66abf44cea60c0e4d61a91c4f3
Matthieu Monsch <ninecoast@gmail.com>
2013-04-20 11:23:39 -0400

    using docopt

--- kit/__main__.py
+++ kit/__main__.py
@@ -1,25 +1,111 @@
 #!/usr/bin/env python
 
-"""Kit command line tool.
+"""Kit: your friendly Flask, Celery, SQLAlchemy toolkit.
 
-There are currently 4 commands available via the command tool, all
-detailed below.
+Usage:
+  kit shell CONF
+  kit server [-dlp PORT] CONF
+  kit worker CONF [(-- [RAW] ...)]
+  kit flower CONF [(-- [RAW] ...)]
+  kit -h | --help | --version
 
-All commands accept an optional argument ``-c, --conf``
-to indicate the path of the configuration file to use. If none is specified
-Kit will search in the current directory for possible matches. If a single
-file ``.cfg`` file is found it will use it.
+Arguments:
+  CONF                  Path to YAML configuration file.
+  RAW                   Options to pass to the underlying command.
+
+Options:
+  -h --help             Show this screen.
+  --version             Show version.
+  -d --debug            Enable in browser debugging and autoreloading.
+  -l --local            Only allow local connections to server.
+  -p PORT --port=PORT   Port to run server on [default: 5000].
+  
 
 """
 
-from kit.commands import parser
+from code import interact
+from docopt import docopt
+from kit import Kit, KitImportError, __version__
+from os import sep
+from os.path import abspath, basename, dirname, join, split, splitext
+from re import findall
+
+
+def run_shell(kit):
+  """Start a shell in the context of the kit.
+
+  This will use IPython if it is available.
+  
+  """
+  context = {'kit': kit}
+  try:
+    import IPython
+  except ImportError:
+    interact(local=context)
+  else:
+    sh = IPython.frontend.terminal.embed.InteractiveShellEmbed()
+    sh(local_ns=context)
+
+def run_server(kit, local, port, debug):
+  """Start a Werkzeug server for the Flask application."""
+  host = '127.0.0.1' if local else '0.0.0.0'
+  kit.flask.run(host=host, port=port, debug=debug)
+
+def run_worker(kit, raw):
+  """Starts a celery worker.
+
+  If no hostname is provided, one will be generated automatically using the
+  project domain and subdomain and current worker count. For example, the first
+  two workers started for project ``my_project`` and configuration ``default``
+  will have respective hostnames:
+
+  * w1.default.my_project
+  * w2.default.my_project
+
+  """
+  name = '%s.%s' % (
+    splitext(split(kit.path)[1])[0],
+    basename(abspath(join(
+      dirname(kit.path),
+      kit.config['root'].rstrip(sep)
+    ))),
+  )
+  worker_pattern = r'w(\d+)\.%s' % (name, )
+  kit_worker_names = [d.keys()[0] for d in kit.celery.control.ping()]
+  worker_numbers = [
+    findall(worker_pattern, worker_name) or ['0']
+    for worker_name in kit_worker_names
+  ]
+  wkn = min(
+    set(range(1, len(worker_numbers) + 2)) -
+    set([int(n[0]) for n in worker_numbers] or [len(worker_numbers) + 2]) 
+  )
+  hostname = 'w%s.%s' % (wkn, name)
+  options = ['worker', '--hostname=%s' % hostname] + raw
+  kit.celery.worker_main(options)
+
+def run_flower(kit, raw):
+  """Start flower worker manager."""
+  options = ['celery', 'flower'] + raw
+  kit.celery.start(options)
 
 def main():
-  """Run command line tool."""
-  parsed_args = parser.parse_args()
-  parsed_args.handler(parsed_args)
-
+  """Command line parser."""
+  arguments = docopt(__doc__, version=__version__)
+  kit = Kit(arguments['CONF'], load_modules=True)
+  if arguments['shell']:
+    run_shell(kit)
+  elif arguments['server']:
+    run_server(
+      kit,
+      local=arguments['--local'],
+      port=int(arguments['--port']),
+      debug=arguments['--debug'],
+    )
+  elif arguments['worker']:
+    run_worker(kit, raw=arguments['RAW'])
+  elif arguments['flower']:
+    run_flower(kit, raw=arguments['RAW'])
 
 if __name__ == '__main__':
   main()
-
--- setup.py
+++ setup.py
@@ -1,31 +1,13 @@
 #!/usr/bin/env python
 
-"""
-Kit
-===
-
-Your friendly Flask, Celery, SQLAlchemy toolkit.
-
-Why use Kit?
-------------
-
-* 1 YAML file for all your configuration options
-* A command line tool to start a development server, Celery workers and more
-* Seamless integration between SQLAlchemy and the rest of your application
-
-How to get Kit?
----------------
-
-.. code:: bash
-
-  pip install kit
-
-You can find the latest version on `Github <https://github.com/mtth/kit>`_.
-documentation and examples!
-
-"""
+"""Kit setup module."""
 
 from setuptools import find_packages, setup
+
+
+def get_long_description():
+  from kit import __doc__
+  return __doc__
 
 def get_version():
   from kit import __version__
@@ -35,7 +17,7 @@ setup(
     name='kit',
     version=get_version(),
     description='Flask, Celery, SQLAlchemy toolkit',
-    long_description=__doc__,
+    long_description=get_long_description(),
     author='Matthieu Monsch',
     author_email='monsch@mit.edu',
     url='http://github.com/mtth/kit/',
@@ -50,6 +32,7 @@ setup(
     install_requires=[
       'blinker',
       'pyyaml',
+      'docopt',
     ],
     entry_points={'console_scripts': ['kit = kit.__main__:main']},
 )
