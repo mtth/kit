8dc0f78f53f0066dae98e7e76aed9a6a2ff68692
Matthieu Monsch <ninecoast@gmail.com>
2013-04-29 09:12:28 -0400

    api fix

--- kit/ext/api.py
+++ kit/ext/api.py
@@ -203,14 +203,14 @@ class View(_View):
   subviews = []
 
   @classmethod
-  def register_view(cls, blueprint):
+  def register_view(cls):
     """Create the URL routes for the view.
     
     Standard :class:`kit.util.View` implementation plus subview support.
     
     """
 
-    super(View, cls).register_view(blueprint)
+    super(View, cls).register_view()
 
     if cls.subviews:
       model = cls.__model__
